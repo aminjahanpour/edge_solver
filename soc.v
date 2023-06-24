1a408afacc23e173dfbadd5577da3be4df82d731
amin <amin.jahanpour@gmail.com>
2023-06-24 15:07:55 -0400

    working on the demo bug: regular codes works now

--- soc.v
+++ soc.v
@@ -232,7 +232,7 @@ module SOC (
 	memory #(
 		.mem_width(register_width),
 		.mem_depth(main_memory_depth),
-		.initial_file("/home/amin/bucking_html/risc-v/program.hex")
+		.initial_file("/home/sammy/myproject/risc-v/program.hex")
 
 	) main_mem(
 		.clk(clk),
